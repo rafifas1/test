7e7c80c1a96397a9e09e971b77d9a29d99313d97
rafif <rafifabdussalam1@gmail.com>
2023-06-14 16:12:48 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pdb
+
+def print_hi(name):
+    pdb.set_trace()
+    a = [1,2,3]
+    var_1 = 1
+    var_2 = 2
+    var_3  = var_1 + var_2
+
+    for i in A:
+        b = i + 1
+        print(i)
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
